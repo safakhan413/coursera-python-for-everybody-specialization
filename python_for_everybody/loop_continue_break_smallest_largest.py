3e76f2e6f5aced2ceae925ca79229d2bd14889e1
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2021-01-09 00:26:06 +0500

    loops try except testing

--- python_for_everybody/loop_continue_break_smallest_largest.py
+++ python_for_everybody/loop_continue_break_smallest_largest.py
@@ -1,3 +1,40 @@
 '''
-
+5.2 Write a program that repeatedly prompts a user for integer numbers until the user
+enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers.
+If the user enters anything other than a valid number catch it with a try/except and put
+out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the
+output below.
 '''
+
+def find_largest_smallest(arr):
+    largest = -1
+    smallest = None
+    # print(arr)
+    for i in arr:
+        if smallest is None:
+            smallest = i
+        if i < smallest:
+            smallest = i
+        if i > largest:
+            largest = i
+    return largest, smallest
+
+
+
+arr = []
+while True:
+    num = input("Enter a number: ")
+
+    try:
+        if num == "done":
+            break
+        else:
+            num = int(num)
+            arr.append(num)
+    except:
+        print('Invalid input')
+
+largest, smallest = find_largest_smallest(arr)
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
